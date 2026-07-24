5e818dc3b1bd2890786f585c2d0aecad6f7af1d5
Harshil Sharma <harshils3107@gmail.com>
2026-07-24 21:35:36 +0530

    Delete 3.DBConnection directory

--- 3.DBConnection/server.js
+++
@@ -1,9 +0,0 @@
-const dns = require('dns')
-dns.setServers(['8.8.8.8', '8.8.4.4']); // Manual Server Setting
-const app = require('./src/app')
-const connectDB = require("./src/DB/db")
-const port = 3000
-connectDB()
-app.listen(port,()=>{
-    console.log(`Server is running at port :${port}`)
-})
--- 3.DBConnection/src/models/notes.models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const notesSchema = new mongoose.Schema({
-    title:{type:String},
-    description:{type:String}
-
-})
-const noteModel = mongoose.model("noteModel",notesSchema)
-module.exports=noteModel
