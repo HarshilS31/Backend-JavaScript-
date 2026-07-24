54961b1ee518f5ee19658bc9daac9debae7a4139
Harshil Sharma <harshils3107@gmail.com>
2026-07-24 21:40:06 +0530

    Delete CodeLogging Project/Backend directory

--- CodeLogging Project/Backend/src/routes/progressRoutes.js
+++
@@ -1,7 +0,0 @@
-import { createProgress,getAllProgress,deleteProgress } from "../controllers/progressControllers.js";
-import { Router } from "express";
-const router = Router()
-router.post("/progress",createProgress)
-router.get("/progress",getAllProgress)
-router.delete("/progress/:id",deleteProgress)
-export default router
