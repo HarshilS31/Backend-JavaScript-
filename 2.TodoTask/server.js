00bb07f24a24c43b0653dab3014352c3ac3bfea5
Harshil Sharma <harshils3107@gmail.com>
2026-07-24 21:36:03 +0530

    Delete 2.TodoTask directory

--- 2.TodoTask/server.js
+++
@@ -1,9 +0,0 @@
-// We will start the server here
-const app = require("./src/app") // we imported the server instance here from app
-const port = 4000
-// app.get("/notes",(req,res)=>{
-//     res.send("Server is working at port 4000")
-// })
-app.listen(port,()=>{// Starting the server
-    console.log("Server is working at port:",port)
-})
